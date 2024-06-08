b6f9b2936e94540c86b988c08bc244d405c91d6f
Vishwa Wijesakare <agvawijesekara@std.appsc.sab.ac.lk>
2024-06-08 20:03:53 +0530

    registration

--- resources/views/include/header.blade.php
+++ resources/views/include/header.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">Navbar</a>
+        <a class="navbar-brand" href="#">Drinker</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -17,7 +17,7 @@
                     <a class="nav-link" href="#">Pricing</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                    <a class="nav-link disabled" aria-disabled="true">About</a>
                 </li>
             </ul>
         </div>
---
+++ resources/views/registration.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts')
+@section('title', 'Registration')
+@section('content')
+
+<div class="container">
+    <h1>Welcome to Drinker!</h1>
+    <p>Please Log In to continue</p>
+    <form class="ms-auto me-auto mt-3" style="width: 500px">
+        <div class="mb-3">
+            <label class="form-label">Full Name</label>
+            <input type="email" class="form-control" name="name">
+        </div>
+        <div class="mb-3">
+            <label class="form-label">E-mail</label>
+            <input type="email" class="form-control" name="email">
+        </div>
+        <div class="mb-3">
+            <label class="form-label">Password</label>
+            <input type="password" class="form-control" name="password">
+        </div>
+        <div class="mb-3">
+            <label class="form-label">Confirm Password</label>
+            <input type="password" class="form-control" name="confirm password">
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+</div>
+
+@endsection
