eaa778252f99ccc87973268af648ad9fa5557411
Vishwa Wijesakare <agvawijesekara@std.appsc.sab.ac.lk>
2024-06-09 22:25:41 +0530

    Auth Manager

---
+++ app/Http/Controllers/Authmanager.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Authmanager extends Controller
+{
+    function login()
+    {
+        return view('login');
+    }
+
+    function registration()
+    {
+        return view('registration');
+    }
+}
