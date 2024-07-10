bc8f2d7a28d787c6e1cdaec1197b447fb3137933
Vishwa Wijesakare <agvawijesekara@std.appsc.sab.ac.lk>
2024-07-10 13:00:23 +0530

    Wine 2

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -8,13 +8,28 @@
     <div>
         <h3 class="text-center">Wines</h3>
         <br>
-        <div class="card" style="width: 18rem; margin: auto;">
-            <img src="{{ asset('images/wine1.jpeg') }}" class="card-img-top" alt="Wines"
-                style="height: 250px; margin: auto;">
-            <div class="card-body">
-                <h5 class="card-title">Banrock Station Shiraz 75Cl</h5>
-                <p class="card-text">A description of the wine can go here.</p>
-                <a href="#" class="btn btn-primary">Buy Now</a>
+        <div class="row">
+            <div class="col-sm-12 col-md-6 mb-4">
+                <div class="card" style="width: 15rem; margin: auto;">
+                    <img src="{{ asset('images/wine1.jpeg') }}" class="card-img-top" alt="Wines"
+                        style="height: 250px; object-fit: cover;">
+                    <div class="card-body">
+                        <h5 class="card-title">Banrock Station Shiraz 75Cl</h5>
+                        <p class="card-text">A description of the wine can go here.</p>
+                        <a href="#" class="btn btn-primary">Buy Now</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-12 col-md-6 mb-4">
+                <div class="card" style="width: 15rem; margin: auto;">
+                    <img src="{{ asset('images/wine2.jpeg') }}" class="card-img-top" alt="Wines"
+                        style="height: 250px; object-fit: cover;">
+                    <div class="card-body">
+                        <h5 class="card-title">Bouchard Grand Conseiller Chardonnay 75Cl</h5>
+                        <p class="card-text">A description of the wine can go here.</p>
+                        <a href="#" class="btn btn-primary">Buy Now</a>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
