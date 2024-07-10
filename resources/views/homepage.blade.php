b019fbb31322ff1dc0864379e8179217042e7df9
Vishwa Wijesakare <agvawijesekara@std.appsc.sab.ac.lk>
2024-07-10 12:42:40 +0530

    Wine Bottle

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -4,13 +4,18 @@
 
 <div class="container">
     <h1>Drinker!</h1>
-    <p>Make your own drinks as you wish!
-    </p>
+    <p>Make your own drinks as you wish!</p>
     <div>
         <h3 class="text-center">Wines</h3>
-        </br>
-        <div>
-            <img src="{{asset('images/wine1.jpeg')}}" alt="Wines" class="img-fluid" width="125px" height="125px">
+        <br>
+        <div class="card" style="width: 18rem; margin: auto;">
+            <img src="{{ asset('images/wine1.jpeg') }}" class="card-img-top" alt="Wines"
+                style="height: 250px; margin: auto;">
+            <div class="card-body">
+                <h5 class="card-title">Banrock Station Shiraz 75Cl</h5>
+                <p class="card-text">A description of the wine can go here.</p>
+                <a href="#" class="btn btn-primary">Buy Now</a>
+            </div>
         </div>
     </div>
 </div>
