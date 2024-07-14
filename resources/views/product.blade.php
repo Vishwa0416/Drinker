1f9e7005e96665c76a1e51fd5b6e5701fa9b8c40
Vishwa Wijesakare <agvawijesekara@std.appsc.sab.ac.lk>
2024-07-14 20:51:51 +0530

    Connect

---
+++ resources/views/product.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts')
+@section('title', 'Product Page')
+@section('content')
+
+<div class="container">
+    <h1>New Page</h1>
+    <p>This is the content of the new page.</p>
+</div>
+
+@endsection
