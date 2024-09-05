f8e983ff3bf707bb1e23a4e29cb8f48c77ce3bb4
Vishwa Wijesakare <agvawijesekara@std.appsc.sab.ac.lk>
2024-09-05 13:18:29 +0530

    Row

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -315,7 +315,7 @@
 
     <div class="row">
         @foreach($shopItems as $item)
-        <div class="col-md-4">
+        <div class="col-md-3">
             <div class="card" style="width: 15rem; margin: auto; height: 400px;">
                 <div class="card mb-4">
                     <img src="{{ $item->image }}" alt="{{ $item->name }}" class="card-img-top"
@@ -325,32 +325,28 @@
                         <p class="card-text">{{ $item->description }}</p>
                         <div class="fade-effect"></div>
                     </div>
-
                 </div>
             </div>
         </div>
         @endforeach
-        <div class="d-flex justify-content-center align-items-center" style="height: 100%; width: 100%; margin: 20px ">
+        <div class="d-flex justify-content-center align-items-center" style="height: 100%; width: 100%; margin: 20px;">
             <a href="#" class="btn btn-outline-primary">See All</a>
         </div>
     </div>
-</div>
+    @endsection
+    <style>
+        .card-body {
+            max-height: 120px;
+            position: relative;
+        }
 
-@endsection
-
-<style>
-    .card-body {
-        max-height: 120px;
-        position: relative;
-    }
-
-    .card-body .fade-effect {
-        position: absolute;
-        bottom: 0;
-        left: 0;
-        width: 100%;
-        height: 50px;
-        background: linear-gradient(to bottom, rgba(255, 255, 255, 0), rgba(255, 255, 255, 1));
-        pointer-events: none;
-    }
-</style>
+        .card-body .fade-effect {
+            position: absolute;
+            bottom: 0;
+            left: 0;
+            width: 100%;
+            height: 50px;
+            background: linear-gradient(to bottom, rgba(255, 255, 255, 0), rgba(255, 255, 255, 1));
+            pointer-events: none;
+        }
+    </style>
