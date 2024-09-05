de802920069ef47d94a3126ee0f5a0b8f33655f3
Vishwa Wijesakare <agvawijesekara@std.appsc.sab.ac.lk>
2024-09-05 19:57:15 +0530

    Whiskey

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -46,6 +46,27 @@
             </div>
         @endforeach
     </div>
+    <h2 class="category">Whiskey</h2>
+    <div class="row">
+        @foreach($shopItems->where('category', 'Whiskey') as $item)
+            <div class="col-md-3">
+                <div class="card" style="width: 15rem; margin: auto; height: 400px;">
+                    <div class="card mb-4">
+                        <img src="{{ $item->image }}" alt="{{ $item->name }}" class="card-img-top"
+                            style="height: 250px; margin:auto;">
+                        <div class="card-body position-relative" style="overflow: hidden;">
+                            <h5 class="card-title">{{ $item->name }}</h5>
+                            <p class="card-text">{{ $item->description }}</p>
+                            <div class="fade-effect"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="d-flex justify-content-center align-items-center" style="height: 100%; width: 100%; margin: 20px;">
+                <a href="#" class="btn btn-outline-primary">See All</a>
+            </div>
+        @endforeach
+    </div>
 </div>
 @endsection
 </div>
