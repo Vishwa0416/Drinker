a667d3544d0cd19d8fb9809dfd0b0c156def2718
Vishwa Wijesakare <agvawijesekara@std.appsc.sab.ac.lk>
2024-09-07 09:58:46 +0530

    products update

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -29,146 +29,36 @@
         </div>
 
     </div>
-</div>
-<div>
-    <h3 class="text-center">Vodka</h3>
-    <br>
+    <h2 class="category">Vodka</h2>
     <div class="row">
-        <div class="col-md-3 mb-4">
-            <div class="card" style="width: 15rem; margin: auto; height: 400px;">
-                <img src="{{ asset('images/vodka1.jpg') }}" class="card-img-top" alt="Absolute Vodka"
-                    style="height: 250px; margin:auto;">
-                <div class="card-body position-relative" style="overflow: hidden;">
-                    <h5 class="card-title">Absolute Vodka Citron 750ML - £10.50</h5>
-                    <p class="card-text">Enjoy the fresh taste of Absolut Vodka Citron. It’s a special vodka with
-                        lemon flavor. It’s made with a lot of care and skill. It’s a good choice for people who want
-                        a great vodka experience.
-
-                        Absolut Vodka Citron has won many awards for its great quality and taste. People who know a
-                        lot about drinks like it a lot. This shows that it’s one of the best vodka brands.
-
-                        You can use Absolut Vodka Citron to make fancy cocktails. You can drink it straight, mix it
-                        into a classic martini, or use it to make citrusy drinks. It’s smooth and tasty. You can
-                        keep it in your bar or at home for any time you want a good drink.</p>
-                    <a href="#" class="btn btn-primary">Buy Now</a>
-                    <div class="fade-effect"></div>
+        @foreach($shopItems->where('category', 'Vodka') as $item)
+            <div class="col-md-3">
+                <div class="card" style="width: 15rem; margin: auto; height: 400px;">
+                    <div class="card mb-4">
+                        <img src="{{ $item->image }}" alt="{{ $item->name }}" class="card-img-top"
+                            style="height: 250px; margin:auto;">
+                        <div class="card-body position-relative" style="overflow: hidden;">
+                            <h5 class="card-title">{{ $item->name }}</h5>
+                            <p class="card-text">{{ $item->description }}</p>
+                            <div class="fade-effect"></div>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="col-md-3 mb-4">
-            <div class="card" style="width: 15rem; margin: auto; height: 400px;">
-                <img src="{{ asset('images/vodka2.jpg') }}" class="card-img-top" alt="Noble Vodka"
-                    style="height: 250px; margin:auto;">
-                <div class="card-body position-relative" style="overflow: hidden;">
-                    <h5 class="card-title">Noblewood Beluga Noble Vodka 700ML - £14.99</h5>
-                    <p class="card-text">Beluga is a very famous and high-quality vodka from Russia. It has a fancy
-                        history and is made with great care. The place where they make it, called the Mariinsk
-                        Distillery, was started in 1900 and is in a clean and natural part of Siberia.</p>
-                    <a href="#" class="btn btn-primary">Buy Now</a>
-                    <div class="fade-effect"></div>
-                </div>
+            <div class="d-flex justify-content-center align-items-center" style="height: 100%; width: 100%; margin: 20px;">
+                <a href="#" class="btn btn-outline-primary">See All</a>
             </div>
-        </div>
-        <div class="col-md-3 mb-4">
-            <div class="card" style="width: 15rem; margin: auto; height: 400px;">
-                <img src="{{ asset('images/vodka3.jpg') }}" class="card-img-top" alt="Finlandia Vodka"
-                    style="height: 250px; margin:auto;">
-                <div class="card-body position-relative" style="overflow: hidden;">
-                    <h5 class="card-title">Finlandia Vodka 750ML - £7.50</h5>
-                    <p class="card-text">Finlandia Vodka is a very good vodka made in Finland. They use natural
-                        things like special Finnish barley and clean glacier water to make it taste dry, light, and
-                        smooth. People have liked the taste of Finlandia Vodka for more than 50 years. You can use
-                        it to make many tasty drinks like a Finlandia Bloody Mary or an Espresso Martini, or even
-                        classic ones like a Vodka Martini or a Cosmo. Finlandia Vodka also helps a charity called
-                        Living Lands and Waters, which works to protect rivers in the United States.</p>
-                    <a href="#" class="btn btn-primary">Buy Now</a>
-                    <div class="fade-effect"></div>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-3 mb-4">
-            <div class="card" style="width: 15rem; margin: auto; height: 400px;">
-                <img src="{{ asset('images/vodka4.jpg') }}" class="card-img-top" alt="Crystal Head Vodka"
-                    style="height: 250px; margin:auto;">
-                <div class="card-body position-relative" style="overflow: hidden;">
-                    <h5 class="card-title">Crystal Head Vodka 50ML - £4.99</h5>
-                    <p class="card-text">Crystal Head uses only the highest-quality ingredients to create unique
-                        expressions of ultra-premium vodka that are completely additive-free. Original, crafted from
-                        locally sourced Canadian corn, Aurora, crafted from English wheat, and Onyx, crafted with
-                        Blue Weber agave.</p>
-                    <a href="#" class="btn btn-primary">Buy Now</a>
-                    <div class="fade-effect"></div>
-                </div>
-            </div>
-        </div>
+        @endforeach
     </div>
 </div>
-<div>
-    <h3 class="text-center">Whiskey</h3>
-    <br>
-    <div class="row">
-        <div class="col-md-3 mb-4">
-            <div class="card" style="width: 15rem; margin: auto; height: 400px;">
-                <img src="{{ asset('images/whiskey.jpeg') }}" class="card-img-top" alt="Jack Daniel's Whiskey"
-                    style="height: 250px; margin:auto;">
-                <div class="card-body position-relative" style="overflow: hidden;">
-                    <h5 class="card-title">Jack Daniel's Black 1000ML - £10.50</h5>
-                    <p class="card-text">This is the whiskey made like our fathers made it a long time ago. It’s
-                        called Jack Daniel’s Old No. 7 Brand Old-Time Tennessee Sour Mash Whiskey, and it’s
-                        different from bourbon. Jack Daniel’s is special because it’s made smooth by dripping it
-                        slowly through 10 feet of charcoal made from sugar maple.</p>
-                    <a href="#" class="btn btn-primary">Buy Now</a>
-                    <div class="fade-effect"></div>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-3 mb-4">
-            <div class="card" style="width: 15rem; margin: auto; height: 400px;">
-                <img src="{{ asset('images/whiskey2.jpeg') }}" class="card-img-top" alt="Gold Label"
-                    style="height: 250px; margin:auto;">
-                <div class="card-body position-relative" style="overflow: hidden;">
-                    <h5 class="card-title">Johnnie Walker Gold Label 700ML - £24.99</h5>
-                    <p class="card-text">Johnnie Walker Gold Label is one of the world’s most iconic whiskies.
-                        Showing flavours of maple syrup, spices and malt on the nose which is then followed by a dry
-                        smokiness and a sweet creamy vanilla mouthfeel. The inherent richness evolves on the finish
-                        with a raisiny, sweet sherry flavour.</p>
-                    <a href="#" class="btn btn-primary">Buy Now</a>
-                    <div class="fade-effect"></div>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-3 mb-4">
-            <div class="card" style="width: 15rem; margin: auto; height: 400px;">
-                <img src="{{ asset('images/whiskey3.jpg') }}" class="card-img-top" alt="Double Black"
-                    style="height: 250px; margin:auto;">
-                <div class="card-body position-relative" style="overflow: hidden;">
-                    <h5 class="card-title">Johnnie Walker Double Black 750ML - £21.50</h5>
-                    <p class="card-text">An interesting twist on Johnnie Walker Black Label, Double Black was rolled
-                        out for general release in 2011 after a successful launch in travel retail. Double Black
-                        adds to the normal Black Label profile by including more Islay whisky and casks that have
-                        been heavily charred, giving a more intense flavour with a greater emphasis on smoke while
-                        keeping true to the sophisticated Johnnie Walker style.</p>
-                    <a href="#" class="btn btn-primary">Buy Now</a>
-                    <div class="fade-effect"></div>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-3 mb-4">
-            <div class="card" style="width: 15rem; margin: auto; height: 400px;">
-                <img src="{{ asset('images/whiskey4.jpg') }}" class="card-img-top" alt="Canadian Club"
-                    style="height: 250px; margin:auto;">
-                <div class="card-body position-relative" style="overflow: hidden;">
-                    <h5 class="card-title">Canadian Club 5 Years 750ML - £14.50</h5>
-                    <p class="card-text">In the glass, Canadian Club has a deep gold color. The nose is very grainy,
-                        with the mid-bodied scent of sweet barley. The flavor is thickly sweet, with a hint of a
-                        spicy bite. The finish is sweet, smooth, and with a bit of spiciness.</p>
-                    <a href="#" class="btn btn-primary">Buy Now</a>
-                    <div class="fade-effect"></div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+
+
+        
+        
+    
+
+
+
 <div>
     <h3 class="text-center">Taquila</h3>
     <br>
