7d9a26975ab188ee3ff069f2d43c7bf8e22bd9b2
Vishwa Wijesakare <agvawijesekara@std.appsc.sab.ac.lk>
2024-09-13 20:59:28 +0530

    carousel

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,7 +1,29 @@
 @extends('layouts')
 @section('title', 'Home Page')
 @section('content')
-<img src="{{ asset('images/homeupdate.png') }}" alt="Home Image" class="home-image">
+
+<div id="homeCarousel" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img src="{{ asset('images/homeupdate.png') }}" class="d-block w-100" alt="Home Image 1">
+        </div>
+        <div class="carousel-item">
+            <img src="{{ asset('images/homeupdate.png') }}" class="d-block w-100" alt="Home Image 2">
+        </div>
+        <div class="carousel-item">
+            <img src="{{ asset('images/homeupdate.png') }}" class="d-block w-100" alt="Home Image 3">
+        </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#homeCarousel" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#homeCarousel" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+    </button>
+</div>
+
 <div class="container">
 
     <h1>Shop Items</h1>
