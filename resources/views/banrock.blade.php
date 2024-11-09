b2d0f4eeda2e13d154d71abf50ece334157fec7f
Vishwa Wijesakare <agvawijesekara@std.appsc.sab.ac.lk>
2024-11-09 19:52:29 +0530

    Buttons

--- resources/views/banrock.blade.php
+++ resources/views/banrock.blade.php
@@ -13,6 +13,8 @@
         <img src="{{ asset('images/wine1.jpeg') }}" alt="Banrock Wine" style="width: 100%; max-width: 500px;">
         <div>
             <p>Banrock Station Shiraz is a bold, full-bodied wine known for its deep flavors and smooth finish. Rich with dark berry notes like blackberry and plum, it has a delightful complexity enhanced by hints of pepper, vanilla, and subtle oak. The aroma combines ripe black cherry with spice, giving it an inviting warmth, while the palate is balanced and smooth, with soft tannins that make it easy to enjoy. Produced by Banrock Station, a winery committed to environmental conservation, this Shiraz is not only a flavorful choice but also an eco-friendly one, aligning with sustainable practices that support wetland and wildlife restoration projects.</p>
+            <h3>7.55</h3>
+            <button>Add to Card</button>
         </div>
     </div>
 </body>
